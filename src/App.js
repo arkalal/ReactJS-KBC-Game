58620e54adef0c6ed6aa57e87630d683c7cea038
arkalal <arkalal.123@gmail.com>
2022-02-01 23:04:22 +0530

    Fully added the timer functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
 import QuizBox from './quiz box/QuizBox';
-import { moneyPyramid } from './MoneyPyramid';
+import Timer from './timer/Timer';
 
 const App = () => {
 
@@ -78,10 +78,28 @@ const App = () => {
         },
     ];
 
+    const moneyPyramid = [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1000" },
+        { id: 6, amount: "$ 2000" },
+        { id: 7, amount: "$ 4000" },
+        { id: 8, amount: "$ 8000" },
+        { id: 9, amount: "$ 16000" },
+        { id: 10, amount: "$ 32000" },
+        { id: 11, amount: "$ 64000" },
+        { id: 12, amount: "$ 125000" },
+        { id: 13, amount: "$ 250000" },
+        { id: 14, amount: "$ 500000" },
+        { id: 15, amount: "$ 1000000" },
+    ].reverse()
+
     useEffect(() => {
         QuestionNumber > 1 &&
             setEarned(moneyPyramid.find((m) => m.id === QuestionNumber - 1).amount);
-    }, [QuestionNumber]);
+    }, [QuestionNumber, moneyPyramid]);
 
     return <div className='app'>
         <div className="main">
@@ -93,7 +111,7 @@ const App = () => {
                     </div></>)
                     :
                     (<> <div className="top">
-                        <div className="timer">30</div>
+                        <div className="timer"> <Timer setStop={setStop} questionNumber={QuestionNumber}></Timer> </div>
                     </div>
 
                         <div className="bottom">
---
+++ src/timer/Timer.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './Timer.css'
+
+const Timer = ({ setStop, questionNumber }) => {
+
+    const [Timer, setTimer] = useState(30);
+
+    useEffect(() => {
+        if (Timer === 0) {
+            setStop(true)
+        }
+        const interval = setInterval(() => {
+            setTimer((current) => current - 1)
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [Timer, setStop])
+
+    useEffect(() => {
+        setTimer(30)
+    }, [questionNumber])
+
+    return Timer;
+};
+
+export default Timer;
