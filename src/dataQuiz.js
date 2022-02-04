1669427b081aaff5e2ea63dd12c5b68c24eeb86d
arkalal <arkalal.123@gmail.com>
2022-02-04 16:31:39 +0530

    added all the 15 questions

--- src/dataQuiz.js
+++ src/dataQuiz.js
@@ -65,4 +65,268 @@ export const data = [
             },
         ],
     },
+    {
+        id: 4,
+        question: "What is React?",
+        answers: [
+            {
+                text: "It is a software",
+                correct: false,
+            },
+            {
+                text: "A Python Library",
+                correct: false,
+            },
+            {
+                text: "It is a Machine",
+                correct: false,
+            },
+            {
+                text: "React is a Javascript Library",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 5,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 6,
+        question: "What is one of the big differences between traditional media and social media?",
+        answers: [
+            {
+                text: "participatory production ",
+                correct: false,
+            },
+            {
+                text: "conversation",
+                correct: false,
+            },
+            {
+                text: "Social Media is a great stuff",
+                correct: false,
+            },
+            {
+                text: "All Dummy options",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 7,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 8,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 9,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 10,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 11,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 12,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 13,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 14,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 15,
+        question: "Who played the character of harry potter in movie?",
+        answers: [
+            {
+                text: "Johnny Deep",
+                correct: false,
+            },
+            {
+                text: "Leonardo Di Caprio",
+                correct: false,
+            },
+            {
+                text: "Denzel Washington",
+                correct: false,
+            },
+            {
+                text: "Daniel Red Cliff",
+                correct: true,
+            },
+        ],
+    },
 ];
